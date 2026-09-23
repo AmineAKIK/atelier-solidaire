eb8fb85b25a03a01a598b114f36d17c6f41b4698
Amine Akik <223266361+AmineAKIK@users.noreply.github.com>
2026-09-23 02:36:56 +0200

    feat(front): structure routing and home view

--- apps/front/src/__tests__/App.spec.ts
+++ apps/front/src/__tests__/App.spec.ts
@@ -1,11 +1,13 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import { RouterView } from 'vue-router'
 
-import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
-    const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+  it('renders the router outlet', () => {
+    const wrapper = shallowMount(App)
+
+    expect(wrapper.findComponent(RouterView).exists()).toBe(true)
   })
 })
